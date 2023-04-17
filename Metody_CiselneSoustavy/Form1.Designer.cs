8f1589096a5f95359c1e2f3f9e04ba3ad74eef9d
dostalm.04 <dostalm.04@spst.eu>
2023-04-17 12:21:34 +0200

    Přidejte soubory projektu.

---
+++ Metody_CiselneSoustavy/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Metody_CiselneSoustavy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(341, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Výstup";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(72, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "101";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(345, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "BIN to DEC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(345, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 26);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "DEC to BIN";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(345, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 26);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "HEX to DEC";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(345, 212);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 26);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "DEC to HEX";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(538, 151);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(124, 26);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "BIN to HEX";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.Location = new System.Drawing.Point(538, 183);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(124, 26);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "HEX to BIN";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Enabled = false;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.radioButton1.Location = new System.Drawing.Point(72, 96);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(113, 24);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Zápis DEC";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Enabled = false;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.radioButton2.Location = new System.Drawing.Point(72, 128);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(106, 24);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Zápis BIN";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Enabled = false;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.radioButton3.Location = new System.Drawing.Point(72, 158);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(113, 24);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Zápis HEX";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 329);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ Metody_CiselneSoustavy/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Metody_CiselneSoustavy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int BinToDEC(string binCislo)
+        {
+          
+               
+              
+       
+
+
+            int mocnina = 1; //  mocnina 2 na 0
+            int d = binCislo.Length;
+            int decCislo = 0;
+
+            for (int i = d - 1; i >= 0; i--)
+            {
+                if (binCislo[i] == '1')
+                {
+                    decCislo += mocnina;
+                }
+                mocnina *= 2;
+            }
+
+            return decCislo;
+        }
+
+        private string DecToBin(int decCislo)
+        {
+            string binCislo = string.Empty;
+
+            
+            while (decCislo > 0)
+            {
+                int zbytek = decCislo % 2;
+                decCislo /= 2;
+                binCislo = zbytek + binCislo;
+            }
+
+
+            return binCislo;
+
+
+           
+        }
+        private int HexToDec(string hexCislo)
+        {
+           
+            int d = hexCislo.Length;
+            int decCislo = 0;
+            int zaklad = 1;
+
+            for (int i = d - 1; i >= 0; i--)
+            {
+                if (hexCislo[i] >= '0' && hexCislo[i] <= '9')
+                {
+                    decCislo += (hexCislo[i] - 48) * zaklad;
+                    zaklad = zaklad * 16;
+                }
+                else if (hexCislo[i] >= 'A' && hexCislo[i] <= 'F')
+                {
+                    decCislo += (hexCislo[i] - 55) * zaklad;
+                    zaklad = zaklad * 16;
+                }
+            }
+
+
+
+            return decCislo;
+        }
+
+        private string DecToHex(int decCislo)
+        {
+            string hex = "";
+            int zbytek = 0;
+            while (decCislo > 0)
+            {
+                zbytek = decCislo % 16;
+                if (zbytek >= 0 && zbytek <= 9)
+                {
+                    hex = hex.Insert(0, zbytek.ToString());
+                }
+                else
+                {
+                    char znakCifry = (char)('A' + zbytek - 10);
+                    hex = hex.Insert(0, znakCifry.ToString());
+
+                }
+
+                decCislo /= 16;
+            }
+            return hex;
+        }
+
+        private string BinToHex(string binCislo)
+        {
+            int delka = binCislo.Length;
+            string hexCislo = "";
+
+            while (delka % 4 != 0)
+            {
+                binCislo = binCislo.Insert(0, "0");
+                delka++;
+            }
+
+           
+            while (delka > 0)
+            {
+                string substring = binCislo.Substring(binCislo.Length - 4, 4);
+                binCislo = binCislo.Remove(binCislo.Length - 4, 4);
+                delka -= 4;
+
+                switch (substring)
+                {
+                    case "0000": hexCislo = hexCislo.Insert(0, "0"); break;
+                    case "0001": hexCislo = hexCislo.Insert(0, "1"); break;
+                    case "0010": hexCislo = hexCislo.Insert(0, "2"); break;
+                    case "0011": hexCislo = hexCislo.Insert(0, "3"); break;
+                    case "0100": hexCislo = hexCislo.Insert(0, "4"); break;
+                    case "0101": hexCislo = hexCislo.Insert(0, "5"); break;
+                    case "0110": hexCislo = hexCislo.Insert(0, "6"); break;
+                    case "0111": hexCislo = hexCislo.Insert(0, "7"); break;
+                    case "1000": hexCislo = hexCislo.Insert(0, "8"); break;
+                    case "1001": hexCislo = hexCislo.Insert(0, "9"); break;
+                    case "1010": hexCislo = hexCislo.Insert(0, "A"); break;
+                    case "1011": hexCislo = hexCislo.Insert(0, "B"); break;
+                    case "1100": hexCislo = hexCislo.Insert(0, "C"); break;
+                    case "1101": hexCislo = hexCislo.Insert(0, "D"); break;
+                    case "1110": hexCislo = hexCislo.Insert(0, "E"); break;
+                    case "1111": hexCislo = hexCislo.Insert(0, "F"); break;
+                    default:
+                        return "Spatne cislo";
+                }
+            }
+
+            return hexCislo.ToString();
+        }
+
+        private string HexToBin(string hexCislo)
+        {
+            string bin = string.Empty;
+            int delka = hexCislo.Length - 1;
+            for (int i = 0; i <= delka; i++)
+            {
+                switch (hexCislo
+                    
+                    [i])
+                {
+                    case '0':
+                        bin = bin.Insert(bin.Length, "0000");
+                        break;
+                    case '1':
+                        bin = bin.Insert(bin.Length, "0001");
+                        break;
+                    case '2':
+                        bin = bin.Insert(bin.Length, "0010");
+                        break;
+                    case '3':
+                        bin = bin.Insert(bin.Length, "0011");
+                        break;
+                    case '4':
+                        bin = bin.Insert(bin.Length, "0100");
+                        break;
+                    case '5':
+                        bin = bin.Insert(bin.Length, "0101");
+                        break;
+                    case '6':
+                        bin = bin.Insert(bin.Length, "0110");
+                        break;
+                    case '7':
+                        bin = bin.Insert(bin.Length, "0111");
+                        break;
+                    case '8':
+                        bin = bin.Insert(bin.Length, "1000");
+                        break;
+                    case '9':
+                        bin = bin.Insert(bin.Length, "1001");
+                        break;
+                    case 'A':
+                    case 'a':
+                        bin = bin.Insert(bin.Length, "1010");
+                        break;
+                    case 'B':
+                    case 'b':
+                        bin = bin.Insert(bin.Length, "1011");
+                        break;
+                    case 'C':
+                    case 'c':
+                        bin = bin.Insert(bin.Length, "1100");
+                        break;
+                    case 'D':
+                    case 'd':
+                        bin = bin.Insert(bin.Length, "1101");
+                        break;
+                    case 'E':
+                    case 'e':
+                        bin = bin.Insert(bin.Length, "1110");
+                        break;
+                    case 'F':
+                    case 'f':
+                        bin = bin.Insert(bin.Length, "1111");
+                        break;
+                    default:
+                        return "\nŠpatné hexadecimální číslo";
+                }
+
+            }
+            return bin;
+        }
+  
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            label1.Text = BinToDEC(s).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            label1.Text = DecToBin(x);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String s = textBox1.Text;
+            label1.Text = HexToDec(s).ToString();
+        }
+    
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            label1.Text = DecToHex(x);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            label1.Text = BinToHex(s);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            label1.Text = HexToBin(s);
+        }
+    }
+}
